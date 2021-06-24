50ddfa2e8051e6e49c685e1749df3c39eea2eb1a
pattytejada <patrick.tejada@gmail.com>
2021-06-24 08:40:09 -0700

    patch
    fixed the github error which deleted previous code

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    """Home page output when taking in request"""
+    return render(request, 'blog/base.html')
+
+def about(request):
+    """Home page output when taking in request"""
+    return render(request, 'blog/base.html')
