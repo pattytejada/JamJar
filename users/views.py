d16d515f0f6a5dc531755088121f6f4245f2286d
pattytejada <patrick.tejada@gmail.com>
2021-06-26 10:57:31 -0700

    User registration
    added a user registration form and app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(request):
+    # input validation for account creation
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save() # saves user input to new user
+            username = form.cleaned_data.get('username') # grabs username for the success message
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form' : form})
