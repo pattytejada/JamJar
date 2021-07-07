95d7c42f8a2d504a563c9e6b8b1f6d85f69a0851
pattytejada <patrick.tejada@gmail.com>
2021-07-07 11:11:11 -0700

    update, create, and delete
    users can now create, update, and delete posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
+from typing import List
+from django.contrib.auth import forms
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView, 
+                                  DetailView, 
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 from .models import Post
 
 def home(request):
@@ -7,3 +15,43 @@ def home(request):
         'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form): #for login mixin, to ensure someone is logged in to post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self): #user passes mixin, ensures only the user can update
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self): #user passes mixin, ensures only the user can delete
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
--- users/models.py
+++ users/models.py
@@ -9,10 +9,10 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs): # resizes uploaded images to 300x300
+        super(Profile, self).save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+            output_size = (300,300)
             img.thumbnail(output_size)
             img.save(self.image.path)
