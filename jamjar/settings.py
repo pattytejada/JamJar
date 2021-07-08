6ddb2c97ee41adb156b8c51f4b5548f2f358117e
pattytejada <patrick.tejada@gmail.com>
2021-07-08 04:11:05 -0700

    password reset
    users can now reset their password if forgotten

--- jamjar/settings.py
+++ jamjar/settings.py
@@ -135,3 +135,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "jamjar.help@gmail.com" #
+EMAIL_HOST_PASSWORD = "JamJ4r3172!"         #
