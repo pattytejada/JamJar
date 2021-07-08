9a5964f1926859444fdbb34a60937b2aad94024e
pattytejada <patrick.tejada@gmail.com>
2021-07-08 04:43:00 -0700

    updated models
    posts now have likes.
    users now have a bio, location, and favorites.
    user profile is now "settings" while profile page is currently being built

--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from blog.models import Post
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    location = models.CharField(max_length=50, null=True, blank=True)
+    bio = models.TextField(max_length=150, null=True, blank=True)
+    favorites = models.ManyToManyField(Post)
 
     def __str__(self):
         return f'{self.user.username} Profile'
