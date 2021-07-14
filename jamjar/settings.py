4ab305b20c5a8de2152072d1148513d55f114f02
pattytejada <patrick.tejada@gmail.com>
2021-07-14 08:54:41 -0700

    Comment app
    Created the comment model and added the app to the project

--- jamjar/settings.py
+++ jamjar/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',    #
     'users.apps.UsersConfig', #
+    'comment.apps.CommentConfig', #
     'crispy_forms', #
     'django.contrib.admin',
     'django.contrib.auth',
